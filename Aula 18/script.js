e195788292d6771efbb3c9c034919ea24aa7b28f
Julia Oliveira <juliaoesrangel@gmail.com>
2025-05-14 20:36:00 -0300

    Add files via upload

---
+++ Aula 18/script.js
@@ -0,0 +1,7 @@
+function nome () {
+    document.getElementById("resposta").innerHTML = 'O nome inserido foi: ' + window.prompt("Qual é o seu nome?");
+}
+
+function abrir () { window.open ("page2.html");}
+
+function envia () { window.alert ( "O nome inserido foi:" + document.getElementById("name").value);}
