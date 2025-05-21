85672df8ca6984adda076fa33e6d785b1d866482
Julia Oliveira <juliaoesrangel@gmail.com>
2025-05-21 20:40:11 -0300

    Add files via upload

---
+++ Aula 20/script.js
@@ -0,0 +1,23 @@
+
+function loop(){
+    for ( var x = 0; x <= 1000; x+=2) {
+        var impar = 999 - x 
+       document.writeln(x)
+       document.writeln(impar)
+   }
+   
+}
+
+function number(){
+    let text = ""
+    let x = 0
+    while( x < 10) {
+        text += "The number is " + x; x++;
+    }
+}
+
+function calculo(){
+
+}
+
+
