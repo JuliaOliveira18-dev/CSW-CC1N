bb0e395d8d4651cd31df72b7f63202b1b22987a8
Julia Oliveira <juliaoesrangel@gmail.com>
2025-05-26 20:34:55 -0300

    Add files via upload

--- Aula 20/script.js
+++ Aula 20/script.js
@@ -1,23 +1,39 @@
 
-function loop(){
-    for ( var x = 0; x <= 1000; x+=2) {
-        var impar = 999 - x 
-       document.writeln(x)
-       document.writeln(impar)
-   }
-   
+function loop() {
+    for (var x = 0; x <= 1000; x += 2) {
+        var impar = 999 - x
+        document.writeln(x)
+        document.writeln(impar)
+    }
+
 }
 
-function number(){
+function number() {
     let text = ""
     let x = 0
-    while( x < 10) {
+    while (x < 10) {
         text += "The number is " + x; x++;
     }
 }
 
-function calculo(){
+function calculo() {
+    var nmr1 = Number(document.getElementById("nmr1").value)
+    var nmr2 = Number(document.getElementById("nmr2").value)
+    var operacao = document.getElementById("operacao").value
+    var resultado
 
+    switch (
+    operacao
+    ) {
+        case "soma": resultado = nmr1 + nmr2;
+            break;
+        case "multiplicacao": resultado = nmr1 * nmr2;
+            break;
+        case "divisao": resultado = nmr1 / nmr2;
+            break;
+        case "subtracao": resultado = nmr1 - nmr2;
+            break;
+    }
+
+    window.alert(resultado)
 }
-
-
